6921cb82fb8ea671873c62ebe9af9be687bd682f
gustavobg <gustavobrittogebrim@gmail.com>
2015-09-24 15:57:54 -0300

    added no-data fix on dist version

--- dist/jstree.js
+++ dist/jstree.js
@@ -13,7 +13,7 @@
 }(function ($, undefined) {
 	"use strict";
 /*!
- * jsTree 3.1.1
+ * jsTree {{VERSION}}
  * http://jstree.com/
  *
  * Copyright (c) 2014 Ivan Bozhanov (http://vakata.com)
@@ -70,7 +70,7 @@
 		 * specifies the jstree version in use
 		 * @name $.jstree.version
 		 */
-		version : '3.1.1',
+		version : '{{VERSION}}',
 		/**
 		 * holds all the default options used when creating new instances
 		 * @name $.jstree.defaults
@@ -3931,7 +3931,7 @@
 				this.settings.core.error.call(this, this._data.core.last_error);
 				return false;
 			}
-			node = old_ins ? old_ins.get_json(obj, { no_id : true, no_data : true, no_state : true }) : obj;
+			node = old_ins ? old_ins.get_json(obj, { no_id : true, no_data : false, no_state : true }) : obj;
 			if(!node) { return false; }
 			if(node.id === true) { delete node.id; }
 			node = this._parse_model_from_json(node, new_par.id, new_par.parents.concat());
